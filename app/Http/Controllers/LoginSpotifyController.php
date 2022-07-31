a652ca473d87c7d3c29da8b44bdcb26f5473db5b
lukmantrue <l699654@gmail.com>
2022-08-01 01:46:36 +0700

    edit loginspotifycontroller

--- app/Http/Controllers/LoginSpotifyController.php
+++ app/Http/Controllers/LoginSpotifyController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginSpotifyController extends Controller
 {
@@ -11,5 +13,34 @@ class LoginSpotifyController extends Controller
         return view('login.index',[
             'title' => 'Login'
         ]);
+    }
+
+    public function authenticate(Request $request){
+
+        $message =[
+            'required'=>':attribute wajib diisi',
+            'min'=>':attribute minimal 5 karakter'
+
+        ];
+        $credentials = $request->validate([
+            'username' => 'required|min:4',
+            'password' => 'required|min:5'
+        ], $message);
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+        // if(Auth::attempt($credentials)){
+        //     $request->session()->regenerate();
+        //     return redirect()->intended('/dashboard');
+        // }
+        return back()->with('loginError', 'Incorrect username and password');   
+    }
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
