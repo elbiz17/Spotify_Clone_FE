df51b76c833c22b95da4ed164c424bba235f6ac2
lukmantrue <l699654@gmail.com>
2022-08-01 01:48:48 +0700

    edit database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory(3)->create();
+        User::create([
+            'username' => 'elbiz',
+            'email' => 'lukmanbudimantrue@gmail.com',
+            'password' =>bcrypt('12345')
+        ]);
+        // User::create([
+        //     'username' => 'noanoa',
+        //     'email' => 'noanoa@gmail.com',
+        //     'password' =>bcrypt('12345')
+        // ]);
     }
 }
